16c8c9dc4bbc945522c44f93c3d46ba65e39a71a
Rat Gabriel Catalin <rat_gabriel@yahoo.com>
2018-11-10 00:15:32 +0200

    Mobile App project

---
+++ Mobile Application/MyMIX/app/src/main/java/com/ratga/mymix/ChatActivity.java
@@ -0,0 +1,23 @@
+package com.ratga.mymix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private MessagesList messagesList;
+    private EditText inputView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        messagesList = findViewById(R.id.messagesList);
+        inputView = findViewById(R.id.input);
+    }
+}
