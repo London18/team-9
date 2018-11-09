bb11689f37fb0267b6d6dfce62b7fb18f9a7c8d0
DELL-XPS\Ierem <ieremiasviorel@yahoo.com>
2018-11-10 01:22:18 +0200

    fix User and Moderator classes

--- ui-web-angular/src/app/shared/models/Moderater.ts
+++ ui-web-angular/src/app/shared/models/Moderater.ts
@@ -1,13 +1,9 @@
-//generated automatically
-import { log } from 'util';
-import { Injectable } from '@angular/core'
-import { User } from './/User'
-export interface Moderater
-{
-	moderaterId : number;
-	userId : number;
-	role : number;
-	creationTime : string;
-	user : User;
+import { User } from './User';
 
+export interface Moderater {
+	moderaterId: number;
+	userId: number;
+	role: number;
+	creationTime: string;
+	user: User;
 }
--- ui-web-angular/src/app/shared/models/User.ts
+++ ui-web-angular/src/app/shared/models/User.ts
@@ -1,14 +1,9 @@
-//generated automatically
-import { log } from 'util';
-import { Injectable } from '@angular/core'
-export interface User
-{
-	userId : number;
-	nickname : string;
-	username : string;
-	password : string;
-	age : number;
-	email : string;
-	creationTime : string;
-
+export interface User {
+	userId: number;
+	nickname: string;
+	username: string;
+	password: string;
+	age: number;
+	email: string;
+	creationTime: string;
 }
