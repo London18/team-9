b359221f542c8f86b9439f9a5f922a25b13ea4f1
DELL-XPS\Ierem <ieremiasviorel@yahoo.com>
2018-11-10 08:41:37 +0200

    Add messages service and integrate with core

--- ui-web-angular/src/app/core/messages.service.ts
+++ ui-web-angular/src/app/core/messages.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Message } from '../shared/models/Message';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ServerUrl } from './ServerUrl';
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +15,35 @@ export class MessagesService {
   welcomeMessage1: Message = this.createMessage('Hello! I am Sam, the friendly bot!');
   welcomeMessage2: Message = this.createMessage('How can I help you today?');
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.messages$ = new BehaviorSubject<Message[]>(null);
     this.messages = new Array<Message>();
-    setTimeout(() => { this.messages.push(this.welcomeMessage1); this.messages$.next(this.messages); setTimeout(() => { this.messages.push(this.welcomeMessage2); this.messages$.next(this.messages); }, 2000); }, 2000);
+    /* setTimeout(() => {
+      this.messages.push(this.welcomeMessage1);
+      this.messages$.next(this.messages);
+      setTimeout(() => {
+        this.messages.push(this.welcomeMessage2); this.messages$.next(this.messages);
+      }, 2000);
+    }, 2000); */
+  }
 
+  public sendMessage(messageText: string, userId: number, sessionId: number) {
+    var message = new Message();
+    message.userId = userId;
+    message.sessionId = sessionId;
+    message.value = messageText;
+    return this.httpClient.post(ServerUrl.GetUrl() + 'Messages.php?cmd=addMessage', message);
   }
 
   private createMessage(messageText: string): Message {
     var message = new Message();
     message.value = messageText;
     message.userId = 0;
-    message.session.userId = 1;
+    message.session.userId = 0;
     return message;
+  }
+
+  public getMessagesBySessionId(sessionId: number) {
+    return this.httpClient.get<Message[]>(ServerUrl.GetUrl() + "Messages.php?cmd=getMessagesBySessionId&sessionId=" + sessionId);
   }
 }
