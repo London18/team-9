30f6caf09606619e85c9d782a991cc01150e2e2d
Rat Gabriel Catalin <rat_gabriel@yahoo.com>
2018-11-10 12:00:32 +0200

    Cleaned dead code in the voice interaction bot

--- desktop_chat_app/main.py
+++ desktop_chat_app/main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from gtts import gTTS
-import pygame
 import speech_recognition as sr
 import requests
 import pyttsx3
@@ -13,41 +11,6 @@ r = sr.Recognizer()
 createSessionUrl = 'http://35.242.228.41/Sessions.php?cmd=addSession&userId='
 createMessageUrl = 'http://35.242.228.41/Messages.php?cmd=addMessage'
 getMessagesUrl = 'http://35.242.228.41/Messages.php?cmd=getMessagesBySessionId&sessionId='
-
-
-def convert(data):
-    harvard = sr.AudioFile(data)
-    output = None
-    with harvard as source:
-        audio = r.record(source)
-        output = r.recognize_google(audio)
-    return output
-
-
-def test_convert_audio_to_text():
-    harvard = sr.AudioFile('harvard.wav')
-    with harvard as source:
-        audio = r.record(source, duration=4)
-        output = r.recognize_google(audio)
-        print(output)
-
-
-@app.route('/message', methods=['GET', 'POST'])
-def message():
-    if requests.method == 'POST':
-        audio_file = requests.files['file']
-        converted = convert(audio_file)
-        return converted
-
-
-def play_message(msg):
-    tts = gTTS(msg)
-    tts.save('audio.mp3')
-    pygame.mixer.init()
-    pygame.mixer.music.load('audio.mp3')
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
 
 
 if __name__ == '__main__':
